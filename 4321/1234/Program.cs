5afe62bc359bce2f4288f501ee5c43007d45ae98
TimurYamchuk <134388477+TimurYamchuk@users.noreply.github.com>
2024-09-26 11:44:04 +0300

    Add files via upload

---
+++ 4321/1234/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество разрядов для сдвига: ");
+        int shift = int.Parse(Console.ReadLine());
+        Console.Write("Введите направление сдвига (1 - влево, 2 - вправо): ");
+        int direction = int.Parse(Console.ReadLine());
+
+        string numberStr = number.ToString();
+        int length = numberStr.Length;
+
+        shift %= length; // Обеспечиваем, что сдвиг не превышает длину числа
+
+        string result = "";
+
+        if (direction == 1)
+        {
+            result = numberStr.Substring(shift) + numberStr.Substring(0, shift);
+        }
+        else if (direction == 2)
+        {
+            result = numberStr.Substring(length - shift) + numberStr.Substring(0, length - shift);
+        }
+        else
+        {
+            Console.WriteLine("Некорректное направление сдвига.");
+            return;
+        }
+
+        Console.WriteLine("Результат сдвига: " + result);
+    }
+}
---
+++ 4321/4321/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()
+            );
+
+        int reversed = 0;
+        int original = number;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        if (original == reversed)
+        {
+            Console.WriteLine("Число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом.");
+        }
+    }
+}
